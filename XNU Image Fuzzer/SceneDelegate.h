3ed5fe164a7d657bf286a9252a1c3fdad892b97a
David Hoyt <h02332@gmail.com>
2024-03-05 19:53:16 -0500

    Add Xcode Project
    Add Xcode Project

---
+++ XNU Image Fuzzer/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  XNU Image Fuzzer
+//
+//  Created by D Hoyt on 2/21/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
